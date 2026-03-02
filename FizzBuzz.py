86f7d48c8b4acda32fef11e27d53e9f931f91a0c
bTroyr <zz3457@columbia.edu>
2026-03-01 21:26:03 -0500

    All tasks completed

---
+++ FizzBuzz.py
@@ -0,0 +1,15 @@
+def FizzBuzz(start, finish):
+
+    result = []
+    
+    for num in range(start, finish + 1):
+        if num % 3 == 0 and num % 5 == 0:
+            result.append("fizzbuzz")
+        elif num % 3 == 0:
+            result.append("fizz")
+        elif num % 5 == 0:
+            result.append("buzz")
+        else:
+            result.append(num)
+    
+    return result
---
+++ getBondDuration.py
@@ -0,0 +1,22 @@
+def getBondDuration(y, face, couponRate, m, ppy=1):
+
+    rate_per_period = y / ppy          
+    coupon_per_period = face * couponRate / ppy  
+    total_periods = m * ppy          
+    total_pvcf = 0.0                
+    weighted_pvcf = 0.0             
+    
+    for period in range(1, total_periods + 1):
+        if period < total_periods:
+            cash_flow = coupon_per_period
+        else:
+            cash_flow = coupon_per_period + face
+        
+        pvcf = cash_flow / (1 + rate_per_period) ** period
+        
+        total_pvcf += pvcf
+        weighted_pvcf += pvcf * period
+    
+    duration = weighted_pvcf / total_pvcf
+    
+    return round(duration, 2)
